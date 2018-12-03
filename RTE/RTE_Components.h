8a7a943fb9052538a14ce019a8be429924c7f76a
chia18 <44865572+chia18@users.noreply.github.com>
2018-12-03 10:40:07 +0000

    STM32F4demo1
    Setting up pincfg for DMA using M/S chaining for 2 and more devices

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: STM32F411CCUx
+ * RTE configuration: STM32F411CCUx.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "ARMCM4_FP.h"
+
+
+#endif /* RTE_COMPONENTS_H */
